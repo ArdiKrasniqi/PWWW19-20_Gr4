12452efd2f64c8a7ae0f1ce6a8afcb38519f93d8
AtlantikLimani <atlantiklimani1@gmail.com>
2019-12-31 17:02:47 +0100

    Escape

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -1,3 +1,18 @@
+function fillimi(){
+    var doc1 = document.getElementById("leftUpperDiv");
+    var doc2 = document.getElementById("rightUpperDiv");
+    var doc3 = document.getElementById("leftLowerDiv");
+    var doc4 = document.getElementById("rightLowerDiv");
+
+    var vargu = {doc1,doc2,doc3,doc4};
+
+    for(var i = 0;i<vargu.length;i++)
+        vargu[i].addEventListener(onmousedown,vargu[i], trubulloTekstin(vargu[i],vargu));
+
+    window.addEventListener("load",sipasOres());    
+    
+        
+}
 function sipasOres(){
     var rez = "blue";
     var ora = new Date().getHours();
@@ -12,4 +27,12 @@ function sipasOres(){
     var trupi = document.getElementById("bdy");
     trupi.style.backgroundColor=rez;
 }
-    window.addEventListener("load",sipasOres());
+
+function trubulloTekstin(a,vargu){
+    for(var i = 0;i<vargu.length;i++)
+        if(vargu[i]!=a)
+            vargu[i].style.filter.blur="5px";
+}
+
+    window.addEventListener("load",fillimi);
+        
