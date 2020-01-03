4b12c5a69587bce8a063741e5d037f1e0df40037
Ardi Krasniqi <ardi.krasniqi99@gmail.com>
2020-01-03 17:34:52 +0100

    add function slider and handling validation

--- js/index.js
+++ js/index.js
@@ -2,3 +2,57 @@ if (getSession('token') || getCookie('token')) {
     document.getElementById("login").href = 'logout.html'
     document.getElementById("login").innerHTML = 'LOGOUT'
 }
+var index = 0;
+var indexText = 0;
+slider();
+sliderText();
+function slider() {
+    var img = ["images/chevron-left-b.png", "images/960x450.png", "images/chevron-right-b.png"];
+    var sliderTitle = ["Aliquatjusto quisque one",
+        "Aliquatjusto quisque two",
+        "Aliquatjusto quisque three"
+    ];
+    var sliderText = ["Left ipsum dolor sit amet, consectetur adipisicing elit.Distinctio quidem pariatur architecto inventore sint consequuntur earum beatae voluptate unde left.",
+        "Center ipsum dolor sit amet, consectetur adipisicing elit.Distinctio quidem pariatur architecto inventore sint consequuntur earum beatae voluptate unde center.",
+        "Right ipsum dolor sit amet, consectetur adipisicing elit.Distinctio quidem pariatur architecto inventore sint consequuntur earum beatae voluptate unde right."
+    ];
+    document.getElementById("rightSide").innerHTML = '<img class="rightImg" src="' + img[index] + '"alt="" />'
+    document.getElementById("sliderTitle").innerHTML = sliderTitle[index];
+    document.getElementById("sliderText").innerHTML = sliderText[index];
+}
+
+function sliderText() {
+    var centerText = ["Left ipsum dolor sit amet, consectetur adipisicing elit.Distinctio quidem pariatur architecto inventore sint consequuntur earum beatae voluptate unde left.",
+        "Center ipsum dolor sit amet, consectetur adipisicing elit.Distinctio quidem pariatur architecto inventore sint consequuntur earum beatae voluptate unde center.",
+        "Right ipsum dolor sit amet, consectetur adipisicing elit.Distinctio quidem pariatur architecto inventore sint consequuntur earum beatae voluptate unde right."
+    ];
+    document.getElementById("centerText").innerHTML = '<q>' + centerText[indexText] + '</q>'
+}
+
+function next() {
+    event.preventDefault();
+    index = (index + 1) % 3;
+    slider();
+}
+
+function previous() {
+    event.preventDefault();
+    index = (index - 1) % 3;
+    if (index < 0) {
+        index += 3
+    }
+    slider();
+}
+
+function nextText() {
+    indexText = (indexText + 1) % 3;
+    sliderText();
+}
+
+function previousText() {
+    indexText = (indexText - 1) % 3;
+    if (indexText < 0) {
+        indexText += 3
+    }
+    sliderText();
+}
--- js/signUp.js
+++ js/signUp.js
@@ -7,6 +7,13 @@ function signUp() {
     event.preventDefault();
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    resetValidation();
+    if (password != confirmPassword) {
+        document.getElementById("passwordMatch").innerHTML =
+            "<p style='color: red;font-size: 16px;'>Password doesn't match!</p>";
+        return;
+    }
     var object = {
         email: email,
         password: password
@@ -15,7 +22,20 @@ function signUp() {
     if (!storedUsers) {
         storedUsers = new Array();
     }
+    const userEmail = storedUsers.find(item => item.email == email);
+    if (userEmail) {
+        document.getElementById("emailTaken").innerHTML =
+            "<p style='color: red;font-size: 16px;'>This email is taken!</p>";
+        return;
+    }
     storedUsers.push(object);
     localStorage.setItem("users", JSON.stringify(storedUsers));
     location.href = 'login.html';
 }
+
+function resetValidation() {
+    document.getElementById("emailTaken").innerHTML = ""
+    document.getElementById("passwordMatch").innerHTML = ""
+    document.getElementById("password").value = "";
+    document.getElementById("confirmPassword").value = "";
+} 
